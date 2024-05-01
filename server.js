b212b5dc06e6e3d01fa5180f04deaac97a003b78
Srivatsan <vatsan1993@gmail.com>
2024-05-01 22:10:26 +0530

    finished phase 5

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const http = require('http');
-const fs = require("fs");
+const fs = require('fs');
 
 const dogs = [
   {
     dogId: 1,
     name: 'Fido',
-    age: 2
+    age: 2,
   },
   {
     dogId: 2,
     name: 'Fluffy',
-    age: 10
-  }
+    age: 10,
+  },
 ];
 
 let nextDogId = 3;
@@ -22,50 +22,49 @@ function getNewDogId() {
   return newDogId;
 }
 
-
 function getContentType(fileName) {
-  const ext = fileName.split(".")[1];
+  const ext = fileName.split('.')[1];
   switch (ext) {
-    case "jpg":
-    case "jpeg":
-      return "image/jpeg";
-    case "png":
-      return "image/png";
-    case "css":
-      return "text/css";
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg';
+    case 'png':
+      return 'image/png';
+    case 'css':
+      return 'text/css';
     default:
-      return "text/plain";
+      return 'text/plain';
   }
 }
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
-  if (req.method === "GET" && req.url.startsWith('/assets')) {
-    const assetPath = req.url.split("/assets")[1];
+  if (req.method === 'GET' && req.url.startsWith('/assets')) {
+    const assetPath = req.url.split('/assets')[1];
     try {
-      const resBody = fs.readFileSync("./assets" + assetPath);
+      const resBody = fs.readFileSync('./assets' + assetPath);
       res.statusCode = 200;
-      res.setHeader("Content-Type", getContentType(assetPath));
+      res.setHeader('Content-Type', getContentType(assetPath));
       res.write(resBody);
       return res.end();
     } catch {
-      console.error("Cannot find asset", assetPath, "in assets folder");
+      console.error('Cannot find asset', assetPath, 'in assets folder');
     }
   }
 
-  let reqBody = "";
-  req.on("data", (data) => {
+  let reqBody = '';
+  req.on('data', (data) => {
     reqBody += data;
   });
 
   // When the request is finished processing the entire body
-  req.on("end", () => {
+  req.on('end', () => {
     // Parsing the body of the request
     if (reqBody) {
       req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .split('&')
+        .map((keyValuePair) => keyValuePair.split('='))
+        .map(([key, value]) => [key, value.replace(/\+/g, ' ')])
         .map(([key, value]) => [key, decodeURIComponent(value)])
         .reduce((acc, [key, value]) => {
           acc[key] = value;
@@ -77,11 +76,11 @@ const server = http.createServer((req, res) => {
     // route handlers
     // GET /
     if (req.method === 'GET' && req.url === '/') {
-      const htmlPage = fs.readFileSync("./views/index.html", 'utf-8');
+      const htmlPage = fs.readFileSync('./views/index.html', 'utf-8');
       const resBody = htmlPage;
 
       res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
+      res.setHeader('Content-Type', 'text/html');
       res.write(resBody);
       return res.end();
     }
@@ -89,11 +88,29 @@ const server = http.createServer((req, res) => {
     // Phase 1: GET /dogs
     if (req.method === 'GET' && req.url === '/dogs') {
       // Your code here
+      const htmlPage = fs.readFileSync('./views/dogs.html', 'utf-8');
+      let resBody = htmlPage;
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      let dogNamesHtml = '';
+      for (let dog of dogs) {
+        dogNamesHtml += `<li>${dog.name}</li>`;
+      }
+      resBody = resBody.replace('#{dogsList}', dogNamesHtml);
+      console.log(resBody);
+      res.write(resBody);
+      return res.end();
     }
 
     // Phase 2: GET /dogs/new
     if (req.method === 'GET' && req.url === '/dogs/new') {
       // Your code here
+      let createDogHtml = fs.readFileSync('./views/create-dog.html');
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(createDogHtml);
+      return res.end();
     }
 
     // Phase 3: GET /dogs/:dogId
@@ -101,14 +118,32 @@ const server = http.createServer((req, res) => {
       const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId));
         // Your code here
+        let dogDetailsHtml = fs.readFileSync(
+          './views/dog-details.html',
+          'utf-8'
+        );
+        let resHtml = dogDetailsHtml;
+        resHtml = resHtml.replaceAll('#{name}', dog.name);
+        resHtml = resHtml.replace('#{age}', dog.age);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(resHtml);
+        return res.end();
       }
     }
 
     // Phase 4: POST /dogs
     if (req.method === 'POST' && req.url === '/dogs') {
       // Your code here
+      let dogId = getNewDogId();
+      dogs.push({ dogId: dogId, ...req.body });
+      res.statusCode = 302;
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Location', `/dogs/${dogId}`);
+      return res.end();
     }
 
     // Phase 5: GET /dogs/:dogId/edit
@@ -116,8 +151,31 @@ const server = http.createServer((req, res) => {
       const urlParts = req.url.split('/');
       if (urlParts.length === 4 && urlParts[3] === 'edit') {
         const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId));
         // Your code here
+        let notFoundHtml = fs.readFileSync('./views/404.html');
+
+        if (dog) {
+          let dogEditHtml = fs.readFileSync('./views/edit-dog.html', 'utf-8');
+          let resHtml = dogEditHtml;
+          resHtml = resHtml.replace('#{dogId}', dog.dogId);
+          resHtml = resHtml.replace('#{name}', dog.name);
+          resHtml = resHtml.replace('#{age}', dog.age);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.write(resHtml);
+          return res.end();
+        } else {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.write(notFoundHtml);
+          return res.end();
+        }
+      } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(notFoundHtml);
+        return res.end();
       }
     }
 
@@ -126,18 +184,17 @@ const server = http.createServer((req, res) => {
       const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId));
         // Your code here
       }
     }
 
     // No matching endpoint
-    const htmlPage = fs.readFileSync("./views/error.html", 'utf-8');
-    const resBody = htmlPage
-      .replace(/#{message}/g, 'Page Not Found');
+    const htmlPage = fs.readFileSync('./views/error.html', 'utf-8');
+    const resBody = htmlPage.replace(/#{message}/g, 'Page Not Found');
 
     res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
+    res.setHeader('Content-Type', 'text/html');
     res.write(resBody);
     return res.end();
   });
